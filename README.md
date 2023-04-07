ec58109bc39abb3cdfce6a9267d5963756ed3daa
Marius Visan <vsnmrs@hotmail.com>
2023-04-07 18:44:24 +0300

    print a gradient output to console, can be redirected to a file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ray-tracer
 Path tracer in C++
+
+By default, the program print the output to the console. To redirect it to a file use:
+     build\Release\RayTracer.exe > image.ppm
--- RayTracer.cpp
+++ RayTracer.cpp
@@ -2,5 +2,24 @@
 
 int main()
 {
+	constexpr int image_width = 256;
+	constexpr int image_height = 256;
 
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	for (int height = image_height - 1; height >= 0; --height)
+	{
+		for (int width = 0; width < image_width; ++width)
+		{
+			auto r = (double)width / (image_width - 1);
+			auto g = (double)height / (image_height - 1);
+			auto b = 0.25;
+
+			int ir = static_cast<int>(255.999 * r);
+			int ig = static_cast<int>(255.999 * g);
+			int ib = static_cast<int>(255.999 * b);
+
+			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+		}
+	}
 }
