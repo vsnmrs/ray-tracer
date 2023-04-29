ecc8dd5fcd4b0a7143370ed6f5b8b04f400b6954
Marius Visan <vsnmrs@hotmail.com>
2023-04-29 12:19:50 +0300

    use the new Vector3 class

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # ray-tracer
 Path tracer in C++
 
+File format used is ppm (Encoded as ASCII).
+
 By default, the program print the output to the console. To redirect it to a file use:
+- In cmd:
     build\Release\RayTracer.exe > image.ppm
-    
-File format used is ppm
+
+- In PowerShell(because by default it encodes in Unicode):
+    build\Release\RayTracer.exe | Out-File -FilePath image.ppm -Encoding ASCII
--- RayTracer.cpp
+++ RayTracer.cpp
@@ -4,25 +4,26 @@
 
 int main()
 {
-	constexpr int image_width = 512;
-	constexpr int image_height = 512;
+	constexpr int image_width = 256;
+	constexpr int image_height = 256;
 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	for (int height = image_height - 1; height >= 0; --height)
 	{
-		//we use std::cout to save the output to a file, so we use the error output stream for anything we want to print to console and shouldn't be in the image
+		//std::cout is used to save the output to a file, so the error output stream must be used for anything that have to be printed to console and shouldn't be in the image
 		std::cerr << "\rScan lines remaining: " << height << ' ' << std::flush;
 
 		for (int width = 0; width < image_width; ++width)
 		{
-			auto r = (double)width / (image_width - 1);
-			auto g = (double)height / (image_height - 1);
-			auto b = 0.25;
+			Vector3 vec{};
+			vec[0] = (double)width / (image_width - 1);
+			vec[1] = (double)height / (image_height - 1);
+			vec[2] = 0.25;
 
-			int ir = static_cast<int>(255.999 * r);
-			int ig = static_cast<int>(255.999 * g);
-			int ib = static_cast<int>(255.999 * b);
+			int ir = static_cast<int>(255.999 * vec[0]);
+			int ig = static_cast<int>(255.999 * vec[1]);
+			int ib = static_cast<int>(255.999 * vec[2]);
 
 			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
 		}
